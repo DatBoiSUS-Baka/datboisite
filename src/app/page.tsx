66c8a042f4bd85c452909fb69c5e0af7d9da8b40
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-07-05 23:01:03 +0700

    EDIT: Fixed some styling for the main content in app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,15 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
       {/* Main Section */}
-        <h1 className="text-6xl">DatBoiSite</h1>
-
-        <div className="row-start-2 items-center justify-items-center">
-
+        <div className='row-start-1'>
+          <h1 className="text-6xl">DatBoiSite</h1>
           <h2 className="text-2xl text-center">A Personal Portfolio Website</h2>
+        </div>
+
+        <div className="row-start-2 items-center flex flex">
+
           <img
-          className="rounded-md justify-items-center"
+          className="rounded-md"
           src="/personal-photo.jpg"
           alt="Aphoey's profile photo"
           width={368}
