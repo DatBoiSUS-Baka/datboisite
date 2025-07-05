3483812e1c5dc50a340b0a1b9a296765c50da290
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-07-05 21:30:49 +0700

    Clean up some indentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,18 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
       {/* Main Section */}
+        <h1 className="text-6xl">DatBoiSite</h1>
 
         <div className="row-start-2 items-center justify-items-center">
 
           <h2 className="text-2xl text-center">A Personal Portfolio Website</h2>
-        <img
+          <img
           className="rounded-md justify-items-center"
-         src="/personal-photo.jpg"
-         alt="Aphoey's profile photo"
-         width={368}
-         height={197}
-        />
+          src="/personal-photo.jpg"
+          alt="Aphoey's profile photo"
+          width={368}
+          height={197}
+          />
 
         </div>
 
